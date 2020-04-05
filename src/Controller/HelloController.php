c4c57769107c616efc2bce296b1e39964743f841
k1012g <kuri.ladybaby1012@gmail.com>
2020-04-05 13:41:35 +0900

    section 2-3 done

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,15 +4,18 @@
 	use App\Controller\AppController;
 
 	class HelloController extends AppController {
-		public $autoRender = false;
+		// public $autoRender = false;
 
-		private $data = [
-			['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
-			['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
-			['name' => 'sachiko', 'mail' => 'sachiko@happy', 'tel' => '0970-777-777']
-		];
-
+		// private $data = [
+		// 	['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
+		// 	['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
+		// 	['name' => 'sachiko', 'mail' => 'sachiko@happy', 'tel' => '0970-777-777']
+		// ];
 		public function index() {
+
+			$this->viewBuilder()->autoLayout(false);
+			$values = ['title' => 'Hello!', 'message' => 'This is message!'];
+			$this->set($values);
 
 
 			// クエリパラメータ
@@ -28,11 +31,11 @@
 			// echo "</body></html>";
 
 			// JSONでの出力
-			$id = 0;
-			if (isset($this->request->query['id'])) {
-				$id = $this->request->query['id'];
-			}
-			echo json_encode($this->data[$id]);
+			// $id = 0;
+			// if (isset($this->request->query['id'])) {
+			// 	$id = $this->request->query['id'];
+			// }
+			// echo json_encode($this->data[$id]);
 		}
 	}
 ?>
