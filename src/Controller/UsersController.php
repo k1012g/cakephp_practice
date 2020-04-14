cb8e42cdb78e10554ece8d280dbc3a8f59f7f734
k1012g <kuri.ladybaby1012@gmail.com>
2020-04-14 18:16:06 +0900

    section 6-2 done

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Event\Event;
 
 /**
  * Users Controller
@@ -12,6 +14,61 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+
+    public function initialize() {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Flash');
+        $this->loadComponent('Auth',[
+            'authorize' => ['Controller'],
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginRedirect' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Users',
+                'action' => 'logout'
+            ],
+            'authError' => 'ログインしてください。',
+        ]);
+    }
+
+    function login() {
+        if ($this->request->isPost()) {
+            $user = $this->Auth->identify();
+            if (!empty($user)) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('ユーザー名かパスワードが間違っています。');
+        }
+    }
+
+    public function logout() {
+        $this->request->session()->destroy();
+        return $this->redirect($this->Auth->logout());
+    }
+
+    public function beforeFilter(Event $event) {
+        parent::beforeFilter($event);
+        $this->Auth->allow(['login']);
+    }
+
+    public function isAuthorized($user = null) {
+        if ($user['role'] === 'admin') {
+            return true;
+        } else {
+            return false;
+        }
+    }
     /**
      * Index method
      *
